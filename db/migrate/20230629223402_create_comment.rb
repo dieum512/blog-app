3a3720f7a0e504d17d1d6e59a7af9e557bcb82d4
dieum512 <akonkwadieum222@gmail.com>
2023-06-30 00:34:44 +0200

    Updathe the Comment migration

--- db/migrate/20230629223402_create_comment.rb
+++ db/migrate/20230629223402_create_comment.rb
@@ -1,7 +1,7 @@
 class CreateComment < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author, foreign_key: { to_table: :users, column: :author_id }
+      t.references :user, foreign_key: { to_table: :users, column: :author_id }
       t.references :post, foreign_key: { to_table: :posts, column: :post_id }
       t.text :text
 

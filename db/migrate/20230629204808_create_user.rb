9376df64c1896fe0149f458e03cca75cd428c5f1
dieum512 <akonkwadieum222@gmail.com>
2023-06-29 23:33:17 +0200

    Remove the dupplicated code

--- db/migrate/20230629204808_create_user.rb
+++ db/migrate/20230629204808_create_user.rb
@@ -4,13 +4,10 @@ class CreateUser < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :photo, null: false, default: '', limit: 2048
       t.text :bio
-      t.datetime :updated_at
-      t.datetime :created_at
       t.integer :posts_counter, default: 0
 
       t.timestamps
     end
 
-    add_index :users, :id, unique: true
   end
 end

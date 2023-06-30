55c014eb991109ad7da856bb0332e51749abe98e
dieum512 <akonkwadieum222@gmail.com>
2023-06-30 23:30:16 +0200

    Fix the linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,14 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-    after_create :update_post_comments_counter
-    after_destroy :decrement_post_comment_counter
+  belongs_to :user
+  belongs_to :post
+  after_create :update_post_comments_counter
+  after_destroy :decrement_post_comment_counter
 
-    def update_post_comments_counter
-        post.update(comments_count: post.comments.count)
-    end
+  def update_post_comments_counter
+    post.update(comments_count: post.comments.count)
+  end
 
-    def decrement_post_comment_counter
-        post.decrement!(:comments_count)
-    end
-endpdate_post_likes_counter
+  def decrement_post_comment_counter
+    post.decrement!(:comments_count)
+  end
+end

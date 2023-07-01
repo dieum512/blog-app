66ac988928325b991d648fd4174818bf116c8081
dieum512 <akonkwadieum222@gmail.com>
2023-07-01 15:43:01 +0200

    Add validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :name, presence: true
+  validates :photo, presence: true
+  validates :posts_counter,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0,
+              message: 'must be an integer greater than or equal to zero'
+            },
+            on: :create
+
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end

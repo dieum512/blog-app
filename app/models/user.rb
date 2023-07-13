b2879e73626a59cb2b1ce32d9b37b54116605714
dieum512 <akonkwadieum222@gmail.com>
2023-07-13 20:53:50 +0200

    Update the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'user_id'
   has_many :comments, foreign_key: 'user_id'
-  has_many :likes
+  has_many :likes, foreign_key: 'user_id'
 
   validates :name, presence: true
   validates :photo, presence: true

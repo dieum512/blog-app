1fa0e1bd7a8b0f08d4473e2eb6f64f300377efee
dieum512 <akonkwadieum222@gmail.com>
2023-07-13 20:52:56 +0200

    Update the like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, foreign_key: 'post_id'
   after_create :update_post_likes_counter
 

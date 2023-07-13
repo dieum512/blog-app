741b44ff21da79ee9056e8bcab0c5451c6a2376a
dieum512 <akonkwadieum222@gmail.com>
2023-07-13 20:52:22 +0200

    Update the comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,17 +1,17 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
   after_create :update_post_comments_counter
   after_destroy :decrement_post_comment_counter
 
-  validates :text, presence: true
-  validates :user_id, presence: true
+  # validates :text, presence: true
+  # validates :user_id, presence: true
 
   def update_post_comments_counter
-    post.update(comments_count: post.comments.count)
+    post.update(comments_counter: post.comments.count)
   end
 
   def decrement_post_comment_counter
-    post.decrement!(:comments_count)
+    post.decrement!(:comments_counter)
   end
 end

0dbda5232a1b206015b08a331f7a36bae66d8cae
dieum512 <akonkwadieum222@gmail.com>
2023-07-14 22:41:38 +0200

    Update the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 

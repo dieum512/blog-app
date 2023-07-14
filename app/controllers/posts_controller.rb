15783d0743082581650343e9a5257a7b79b946c4
dieum512 <akonkwadieum222@gmail.com>
2023-07-14 22:40:14 +0200

    Update the post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @users = User.find(params[:user_id])
+    @posts = @users.posts
+    @comments = Comment.where(post_id: @posts.pluck(:id))
   end
 
   def show
-    @post = Post.find_by_id(params[:id])
+    @post = Post.find(params[:id])
+    @users = @post.author
+    @index = @users.posts.order(created_at: :asc).pluck(:id).index(@post.id)
   end
 end

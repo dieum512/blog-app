f76e0a2b9ea7bfa3bd1f30c4c221dfa8f05c695a
dieum512 <akonkwadieum222@gmail.com>
2023-07-15 18:28:30 +0200

    Update the routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'users#index'
   
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
 end

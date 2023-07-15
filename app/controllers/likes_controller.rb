358d780e579c4a460462a33b696a22bc535463d0
dieum512 <akonkwadieum222@gmail.com>
2023-07-15 18:30:11 +0200

    Create the likes controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,14 @@
+class LikesController < ApplicationController
+    def create
+      @post = params[:post_id]
+      @user = @current_user
+      @like = Like.new(author: @user, post_id: @post)
+      if @like.save
+        flash[:success] = 'You liked this post'
+      else
+        flash[:alert] = 'You cannot like this post'
+      end
+  
+      redirect_to user_post_path(@user.id, @post)
+    end
+  end
